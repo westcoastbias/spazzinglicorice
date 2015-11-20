39991ffe9c3db525fd73d725f69bb41145bc4c6f
Lucas Ruprecht <lruprecht@gmail.com>
2015-11-19 16:06:58 -0800

    updated formatting of board.js export statement to match format on newly created user.js
    implemented password hashing and password compare functionality on the user schema (along with a pre-save hook to hash passwords); these features are currently untested, but I'm moving onto that testing now

--- db/user.js
+++ db/user.js
@@ -1,8 +1,8 @@
 // # Mongoose User Model & Schema
-
-
 var mongoose = require('mongoose');
 var db = require('./config.js');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
 
 var userSchema = new mongoose.Schema ({
   id: String,
@@ -11,7 +11,26 @@ var userSchema = new mongoose.Schema ({
   boards: Array
 });
 
+userSchema.pre('save', function ( next ) {
+  this.hashPassword();
+  next();
+});
+
+userSchema.methods.hashPassword = function () {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+    });
+};
+
+userSchema.methods.comparePassword = function (attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
 var User = mongoose.Model('user', userSchema);
 
-module.exports.userModel = User;
+module.exports = User;
 
